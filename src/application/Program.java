cd343cc11e8f0dd9bb5822bbc081ef84c8b74485
Anilton Viganigo Junior <viganigojr@gmail.com>
2020-07-23 22:46:27 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,20 +1,16 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Department obj = new Department(1, "Books");
-		Seller seller = new Seller(21,"Anilton Júnior", "jr@gmail.com", new Date(), 3500.0, obj);
-		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(8);
 		
 		System.out.println(seller);
 	}
