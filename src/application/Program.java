adc5cfac45e54580a983c1d7b0467c29d91cb99c
Anilton Viganigo Junior <viganigojr@gmail.com>
2020-07-24 21:47:05 -0300

    Update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -17,27 +17,31 @@ public class Program {
 		Seller seller = sellerDao.findById(8);
 		System.out.println(seller);
 
-		
 		System.out.println("\n=== Test number two: seller findByDepartment === ");
 		Department department = new Department(4, null);
 		List<Seller> list = sellerDao.findByDepartment(department);
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
-		
+
 		System.out.println("\n=== Test number three: seller findAll === ");
 		list = sellerDao.findAll();
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
-		
-		System.out.println("\n=== Test number three: seller findAll === ");
+
+		System.out.println("\n=== Test number three: seller insert === ");
 		Seller newSeller = new Seller(null, "Quixote", "quixote@hotmail.com", new Date(), 6700.00, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New Id = " + newSeller.getId());
-		
-	}
-		
+
+		System.out.println("\n=== Test number five: seller Update === ");
+		seller = sellerDao.findById(3);
+		seller.setName("Maria Juana");
+		seller.setEmail("mariajuana@gmail.com");
+		sellerDao.update(seller);
+		System.out.println("Update completed!");
+
 	}
 
-
+}
